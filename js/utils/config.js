86af6e9099112670b964bfaeb63853828641a0be
Jacob Friis Strand <hello@jacobfriisstrand.com>
2024-12-11 16:00:19 +0100

    Add files via upload

---
+++ js/utils/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  API_BASE_URL: "http://localhost:8080",
+  ENDPOINTS: {
+    BOOKS: "/books",
+    AUTHORS: "/authors",
+    PUBLISHERS: "/publishers",
+    USERS: "/users",
+    ADMIN: "/admin",
+  },
+  STORAGE_KEYS: {
+    USER_ID: "user_id",
+    IS_ADMIN: "is_admin",
+  },
+  SIMULATE_ERROR: false,
+};
+
+export default CONFIG;
